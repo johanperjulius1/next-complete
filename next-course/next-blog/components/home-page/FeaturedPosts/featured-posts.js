8ec8aa26ee13a18765ff515ffcfbf08920dcf19d
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2024-06-12 11:55:13 +0200

    finishing 359.

---
+++ next-course/next-blog/components/home-page/FeaturedPosts/featured-posts.js
@@ -0,0 +1,9 @@
+import classes from './featured-posts.module.css'
+
+export default function FeaturedPosts() {
+  return (
+    <section className={classes.latest}>
+        <h2>Featured posts</h2>
+    </section>
+  )
+}
---
+++ next-course/next-blog/components/posts/post-item.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './post-item.module.css'
+
+export default function PostItem() {
+  return (
+    <div className={classes.post}>PostItem</div>
+  )
+}
