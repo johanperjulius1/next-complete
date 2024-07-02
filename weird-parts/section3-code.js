6e046f78d7186c10df4283cbbf013fd43bdf5732
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2024-07-02 13:49:51 +0200

    first lesson of section  3

--- weird-parts/section3-code.js
+++ weird-parts/section3-code.js
@@ -15,3 +15,36 @@ var firstNameProperty = "firstname";
 
 console.log(person);
 console.log(person[firstNameProperty]);
+
+// Using variables to access property names and values, also known as 
+// bracket notation or computed property names, offers several benefits:
+
+// more flexible and dynamic code.
+
+// You can use property names that wouldn't be valid with dot notation.
+
+// Programmatic Property Manipulation:
+// Useful when you need to work with properties in a loop or based on some logic.
+
+const properties = ["name", "age", "city"];
+properties.forEach(prop => {
+  console.log(person[prop]);
+});
+
+// -------------------------------
+
+// Working with User Input:
+// When property names come from user input or external data.
+// Internationalization:
+// // Useful for accessing properties based on language codes.
+// Avoiding Naming Conflicts:
+// When property names might conflict with reserved words or existing variable names.
+// Metaprogramming:
+// Facilitates creating or manipulating objects based on runtime conditions.
+
+// Working with APIs:
+// When dealing with APIs that return property names that might not be known in advance.
+
+// Creating Objects with Dynamic Keys:
+// Useful when creating objects where the key names are determined at runtime.
+
