8b798c44b6a8ec46b61bc4ffad0e2598b17fb705
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2024-07-02 16:05:52 +0200

    finishing 365

---
+++ next-course/next-blog/app/posts/[postSlug]/page.js
@@ -0,0 +1,8 @@
+import PostContent from '@/components/posts/single-post/post-content'
+import React from 'react'
+
+export default function SinglePostsPage() {
+  return (
+    <PostContent/>
+  )
+}
